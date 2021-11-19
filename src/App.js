7c1295f657955c1052f22ce3822885c67406b036
wal3sca <walescaambroise@gmail.com>
2021-11-19 17:01:22 -0500

    broken code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import "./App.css";
+import data from "./data/productData";
+import ProductCard from "./components/ProductCard"
+import SelectedProd from "./components/SelectedProduct"
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
+
+
+constructor() {
+  super();
+  this.state = {
+    product: data.productData,
+    selectedProduct: {
+      name: "",
+      price: "",
+      description:
+        "",
+      image: "https://via.placeholder.com/100",
+    }
+  };
+}
+
+handleProductClick= (item) => {
+  this.setState({
+    selectedProduct: item,
+  });
 };
+
+render() {
+  const { selectedProduct } = this.state;
+  const product = this.state.product.map((item) => {
+    return (
+      <ProductCard
+      key={item.id}
+      item={item}
+      handleProductClick={this.handleProductClick}
+          />
+       );
+    });
+      
+    return (
+      <div className="app" id="app-container">
+        <h1>Product List</h1>
+        <SelectedProd prod={selectedProduct} />
+        {SelectedProd({dino: selectedProduct})}
+        <div id="product-list-container">{product}</div>
+      </div>
+      );
+    }
+  }
+
+
 
 export default App;
